d4cd711638ab5e307cd5896c6b6154e5c5f565cd
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2020-12-22 14:52:12 +0200

    work page styling

--- components/icon.js
+++ components/icon.js
@@ -4,18 +4,10 @@ import { icons } from "../lib/icons";
 const Icon = (props) => {
   const iconData = icons[props.icon];
   return (
-    <svg
-      width={`${props.size}px`}
-      height={`${props.size}px`}
-      viewBox="0 0 1024 1024"
-    >
+    <svg viewBox="0 0 1024 1024">
       <path d={iconData.path} fill={iconData.color}></path>
     </svg>
   );
-};
-
-Icon.defaultProps = {
-  size: 20,
 };
 
 export default Icon;
--- pages/work/index.js
+++ pages/work/index.js
@@ -3,7 +3,15 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../../components/layout";
 import RepoList from "../../components/github/repoList";
 import Icon from "../../components/icon";
-import { GITHUB, GITLAB, STACKOVERFLOW, CODEWARS } from "../../lib/icons";
+import {
+  GITHUB,
+  GITLAB,
+  STACKOVERFLOW,
+  CODEWARS,
+  LINKEDIN,
+  TWITTER,
+} from "../../lib/icons";
+import styles from "../../styles/utils.module.scss";
 
 export default function Work() {
   return (
@@ -11,48 +19,98 @@ export default function Work() {
       <Head>
         <title>{siteTitle} - My work</title>
       </Head>
-      <section>
-        <a href="http://www.github.com/altany">
-          <Icon icon={GITHUB} />
-          Github
-        </a>
-        <br />
-        <a href="https://gitlab.com/brief-challenges">
-          <Icon icon={GITLAB} />
-          Gitlab
-        </a>
-        <br />
-        <a href="http://stackoverflow.com/story/tany4">
-          <Icon icon={STACKOVERFLOW} />
-          StackOverlow
-        </a>
-        <br />
-        <Icon icon={CODEWARS} />
-        If you like code challenges, find me at CodeWars:
-        <a href="https://www.codewars.com/users/altany" target="_codewars">
+      <div className={styles.work}>
+        <header>
+          <h1>My work</h1>
+          <h3>Check the links below to find out more about me</h3>
+        </header>
+        <div>
+          <ul className={styles.links}>
+            <li>
+              <a
+                href="http://www.linkedin.com/in/taniapapazaf"
+                target="_linkedin"
+                title="/in/taniapapazaf"
+              >
+                <Icon icon={LINKEDIN} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/_Tany_"
+                target="_twitter"
+                title="@_Tany_"
+              >
+                <Icon icon={TWITTER} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://stackoverflow.com/story/tany4"
+                target="_stackoverflow"
+                title="story/tany4"
+              >
+                <Icon icon={STACKOVERFLOW} />
+              </a>
+            </li>
+          </ul>
+          <ul className={styles.git}>
+            <li>
+              <a
+                href="http://www.github.com/altany"
+                target="_github"
+                title="altany"
+              >
+                <Icon icon={GITHUB} />
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://gitlab.com/brief-challenges"
+                target="_gitlab"
+                title="altany"
+              >
+                <Icon icon={GITLAB} />
+                Gitlab
+              </a>
+            </li>
+          </ul>
+          <section className={styles.codewars}>
+            <p>Do you enjoy code challenges? Find me at CodeWars!</p>
+            <div>
+              <Icon icon={CODEWARS} />
+              <a
+                href="https://www.codewars.com/users/altany"
+                target="_codewars"
+                title="User altany"
+              >
+                <img
+                  src="https://www.codewars.com/users/altany/badges/small"
+                  alt="codewars profile badge"
+                />
+              </a>
+            </div>
+          </section>
+        </div>
+        <section>
+          <h4>Github Languages pie chart</h4>
           <img
-            src="https://www.codewars.com/users/altany/badges/small"
-            alt="codewars profile badge"
+            src="github languages pie chart.png"
+            alt="Github Languages pie chart"
           />
-        </a>
-      </section>
-      <section>
-        <h4>Github Languages pie chart</h4>
-        <img
-          src="github languages pie chart.png"
-          alt="Github Languages pie chart"
-        />
-      </section>
-      <section>
-        <h4>Github API Playground</h4>
-        <p>
-          {
-            "This page contains a list of my Github repos. All the data is retrieved through the "
-          }
-          <a href="https://developer.github.com/v3/">Github API v3</a>
-        </p>
-        <RepoList />
-      </section>
+        </section>
+        <section>
+          <h4>Github API Playground</h4>
+          <p>
+            {
+              "This page contains a list of my Github repos. All the data is retrieved through the "
+            }
+            <a href="https://developer.github.com/v3/">Github API v3</a>
+          </p>
+          <RepoList />
+        </section>
+      </div>
     </Layout>
   );
 }
