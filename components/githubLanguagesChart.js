440bddd9ddbef85781c0ec91e1abb6d127a2ae6c
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2020-12-29 14:55:40 +0200

    First attempt at responsive layout for charts

--- components/githubLanguagesChart.js
+++ components/githubLanguagesChart.js
@@ -1,69 +1,85 @@
 import { language } from "gray-matter";
 import React from "react";
-import { PieChart, Pie, Legend, Tooltip, BarChart, Bar, XAxis } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Tooltip,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
 import useSWR from "swr";
+import styles from "../styles/utils.module.scss";
+
+const RADIAN = Math.PI / 180;
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+  name,
+}) => {
+  const radius = outerRadius + 25;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  return (
+    <text
+      x={x}
+      y={y}
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${name} ${(percent * 100).toFixed(1)}%`}
+    </text>
+  );
+};
 
 export default function Chart() {
-  const fetcher = (url) => fetch(url).then((r) => r.json());
   const { data, error } = useSWR(
     "https://github-api-altany.herokuapp.com/languages",
     fetcher
   );
   if (error) return <div>failed to load chart</div>;
   if (!data) return <div>loading chart data...</div>;
-
   const chartData = data.map(({ language, value }) => ({
     name: language,
     value,
   }));
-  const RADIAN = Math.PI / 180;
-
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-    name,
-  }) => {
-    const radius = outerRadius + 25;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-      <text
-        x={x}
-        y={y}
-        textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {`${name} ${(percent * 100).toFixed(1)}%`}
-      </text>
-    );
-  };
-
   return (
-    <>
-      <PieChart width={600} height={500}>
-        <Pie
-          dataKey="value"
-          isAnimationActive={true}
-          data={chartData}
-          cx={230}
-          cy={230}
-          outerRadius={170}
-          label={renderCustomizedLabel}
-          fill="#79769c"
-          minAngle="1"
-        />
-        <Tooltip />
-      </PieChart>
-      <BarChart width={1000} height={200} data={chartData}>
-        <Bar dataKey="value" fill="#79769c" minPointSize="1" />
-        <XAxis dataKey="name" />
-      </BarChart>
-    </>
+    <div className={styles.chartContainer}>
+      <div className={styles.pieChart}>
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              dataKey="value"
+              isAnimationActive={true}
+              data={chartData}
+              label={renderCustomizedLabel}
+              fill="#79769c"
+              minAngle={1}
+            />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <BarChart data={chartData} layout="vertical">
+            <Bar dataKey="value" fill="#79769c" minPointSize={1} label />
+            <XAxis type="number" />
+            <YAxis type="category" dataKey="name" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
   );
 }
