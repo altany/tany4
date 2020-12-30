f674b8783e39d2e6d3c321c45d8dfee4aba37a73
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2020-12-30 15:58:56 +0200

    add list of repos with given language in tooltip

--- components/github/tooltip.js
+++ components/github/tooltip.js
@@ -1,12 +1,32 @@
 import React from "react";
 import styles from "../../styles/utils.module.scss";
+import fetcher from "../../lib/fetcher";
+import useSWR from "swr";
 
 const Tooltip = ({ active, payload, ...rest }) => {
   if (active) {
     const label = payload[0]?.name;
     const value = payload[0]?.value;
+    const { data, error } = useSWR(
+      `https://github-api-altany.herokuapp.com/repos/language/${label}`,
+      fetcher
+    );
+
     return (
-      <div className={styles.tooltip}>{`Repos with ${label}: (soon...)`}</div>
+      <div className={styles.tooltip}>
+        {error && "failed to load repos"}
+        {!data && "loading repo data..."}
+        {
+          <>
+            <p>Repos with {label}:</p>
+            <ul>
+              {data.map((repo) => (
+                <li>{repo}</li>
+              ))}
+            </ul>
+          </>
+        }
+      </div>
     );
   }
 
