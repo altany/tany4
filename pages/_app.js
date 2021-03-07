a0732f7a1d46a7cf8584ad7dd33b15e63d6634f4
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2021-03-07 23:12:22 +0200

    Remove AMP and complete analytics setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import "../styles/global.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { pageview } from "../lib/analytics";
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
-}
+};
+
+export default App;
