7227cfe2012d2553b5d3daad5858b5310b8f470c
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2021-03-08 18:25:47 +0200

    rename GTM env var

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "../styles/global.scss";
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
-import { GTM_TRACKING_ID } from "../lib/constants";
+import { NEXT_PUBLIC_GTM_TRACKING_ID } from "../lib/constants";
 
 const MyApp = ({ Component, pageProps }) => {
   // Google Tag Manager
   useEffect(() => {
-    console.log(GTM_TRACKING_ID);
+    console.log(NEXT_PUBLIC_GTM_TRACKING_ID);
 
-    TagManager.initialize({ gtmId: GTM_TRACKING_ID });
+    TagManager.initialize({ gtmId: NEXT_PUBLIC_GTM_TRACKING_ID });
   }, []);
 
   return <Component {...pageProps} />;
