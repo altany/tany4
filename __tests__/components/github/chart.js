40561bf867080d420e928a996c0e110a6bb34e91
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2022-06-29 12:47:56 +0100

    Update recharts library

--- __tests__/components/github/chart.js
+++ __tests__/components/github/chart.js
@@ -4,6 +4,11 @@ import Chart from'../../../components/github/chart'
 jest.mock('swr')
 const swr = require('swr').default
 
+jest.mock("react-resize-detector", () => ({children}) => {
+    const MockReactResizeDetector = "mock-react-resize-detector";
+    return <MockReactResizeDetector>{children}</MockReactResizeDetector>;
+  });
+
 it('renders a chart with data and no error unchanged', () => {
     const data=[{language:'Javascript', value:1}, {language:'CSS', value:2}]
     const error=undefined
