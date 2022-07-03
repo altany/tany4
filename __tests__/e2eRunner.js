04d94f877f54ce3393d7b1a5061680e0d9e5ac2d
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2022-07-03 02:00:21 +0100

    improvements

--- __tests__/e2eRunner.js
+++ __tests__/e2eRunner.js
@@ -1,13 +1,13 @@
 const createTestCafe = require('testcafe');
 
+let testcafe
 createTestCafe('localhost', 3001, 3002)
-    .then(testcafe => {
+    .then(testcafeInstance => {
+        testcafe = testcafeInstance
         return testcafe.createRunner()
-        .startApp('PORT=3000 && node server.js', 10000) // serve the static build
-        .run()
-            .then(() => {
-                testcafe.close()
-            })
-            .catch(console.error);
+            .startApp('PORT=3000 node server.js', 1000) // serve the static build
+            .run()
+    }).then(()=>{
+        testcafe.close()
     }).catch(console.error)
    
