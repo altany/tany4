78a809d674fcff67b2fc3d64290da05058d001c4
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2022-07-03 01:08:07 +0100

    put it back and add wait time

--- __tests__/e2eRunner.js
+++ __tests__/e2eRunner.js
@@ -3,6 +3,7 @@ const createTestCafe = require('testcafe');
 createTestCafe('localhost', 3001)
     .then(testcafe => {
         return testcafe.createRunner()
+        .startApp('node server.js', 10000) // serve the static build
         .run()
             .then(() => {
                 testcafe.close()
