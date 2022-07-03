b95e46f44579268ccb4752ad80e2bbb154c14ae8
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2022-07-03 01:25:56 +0100

    try to set the port

--- __tests__/e2eRunner.js
+++ __tests__/e2eRunner.js
@@ -3,7 +3,7 @@ const createTestCafe = require('testcafe');
 createTestCafe('localhost', 3001, 3002)
     .then(testcafe => {
         return testcafe.createRunner()
-        .startApp('node server.js', 10000) // serve the static build
+        .startApp('PORT=3000 && node server.js', 10000) // serve the static build
         .run()
             .then(() => {
                 testcafe.close()
