fd89abb0bff565afe571435406bcb9047207d832
altany <taniapapazaf@gmail.com>
2025-12-09 22:06:57 +0000

    Update Chrome browser path to use chrome: prefix with separate headless and no-sandbox flags

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "browsers": [
-      'path:/app/.chrome-for-testing/chrome-linux64/chrome:headless --no-sandbox'
+      'chrome:/app/.chrome-for-testing/chrome-linux64/chrome --headless --no-sandbox'
     ],
     "clientScripts": [
         { "module": "@testing-library/dom/dist/@testing-library/dom.umd.js" }
