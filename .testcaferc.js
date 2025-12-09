766b112f4b1c14d69cf89b0ff7891b7550b3a565
altany <taniapapazaf@gmail.com>
2025-12-09 22:13:20 +0000

    Add disable-native-automation and disable-features flags to Chrome headless configuration for improved test stability

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "browsers": [
-      'chrome:/app/.chrome-for-testing/chrome-linux64/chrome --headless --no-sandbox'
+      'chrome:/app/.chrome-for-testing/chrome-linux64/chrome --headless --no-sandbox --disable-native-automation --disable-features=LocalNetworkAccessChecks'
     ],
     "clientScripts": [
         { "module": "@testing-library/dom/dist/@testing-library/dom.umd.js" }
