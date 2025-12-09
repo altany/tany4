0af3c3e0351478bca01844631f63b1651449b24a
altany <taniapapazaf@gmail.com>
2025-12-09 17:50:10 +0000

    Remove Chrome automation detection flags from TestCafe configuration

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "browsers": ["chrome:headless --disable-native-automation --disable-features=LocalNetworkAccessChecks"],
+    "browsers": ["chrome:headless"],
     "clientScripts": [
         { "module": "@testing-library/dom/dist/@testing-library/dom.umd.js" }
     ],
