feffae80ffd4c6327c80666b14202e28d60a5477
altany <taniapapazaf@gmail.com>
2025-12-13 16:20:48 +0000

    Update site description to emphasize mentoring and add cache headers for Next.js static assets with far-future expiration dates

--- next.config.js
+++ next.config.js
@@ -19,6 +19,23 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
           },
+          {
+            key: 'Expires',
+            value: 'Thu, 31 Dec 2099 23:59:59 GMT',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+          {
+            key: 'Expires',
+            value: 'Thu, 31 Dec 2099 23:59:59 GMT',
+          },
         ],
       },
     ];
