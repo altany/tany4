83b94b07d8467455cb2702c4c58166fbce601ddc
altany <taniapapazaf@gmail.com>
2026-04-12 02:26:48 +0300

    Track per-page views via GTM dataLayer on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import "../styles/global.scss";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import TagManager from "react-gtm-module";
 import { NEXT_PUBLIC_GTM_TRACKING_ID } from "../lib/constants";
 
 const MyApp = ({ Component, pageProps }) => {
-  // Google Tag Manager
+  const router = useRouter();
+
+  // Initialise GTM once
   useEffect(() => {
     TagManager.initialize({ gtmId: NEXT_PUBLIC_GTM_TRACKING_ID });
   }, []);
+
+  // Send a pageview event to the GTM dataLayer on every route change
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      TagManager.dataLayer({
+        dataLayer: {
+          event: "pageview",
+          page: url,
+        },
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
 
   return <Component {...pageProps} />;
 };
